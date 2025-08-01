a2b1016b4b5ce12752bf3a01f780767d8c29a3da
Wenjian(Michael) Shi <61084115+Micsushi@users.noreply.github.com>
2025-08-01 02:24:05 -0600

    bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-background text-foreground`}>
         <main className="min-h-screen flex flex-col items-center">{children}</main>
       </body>
     </html>
